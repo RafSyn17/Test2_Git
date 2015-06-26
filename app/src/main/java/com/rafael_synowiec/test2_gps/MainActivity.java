8900574d2e52803600f2f177e36b2aadb7bba205
rafael_synowiec <rafael.synowiec@gmail.com>
2015-06-26 07:39:55 +0200

    [refac] removed obsolete import of ActionBarActivity class

--- app/src/main/java/com/rafael_synowiec/test2_gps/MainActivity.java
+++ app/src/main/java/com/rafael_synowiec/test2_gps/MainActivity.java
@@ -1,6 +1,5 @@
 package com.rafael_synowiec.test2_gps;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
